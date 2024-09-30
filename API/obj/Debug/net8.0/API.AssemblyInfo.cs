9cc6972e1eaa1bf6078db7dcafb010698b3d945d
Mathilde PEAUGER <mathilde.peauger@gmail.com>
2024-09-30 23:37:08 +0200

    update database

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7d68d60ae26c29f317ccaf6d0fa97a0f0b5859d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a02b09f05781dfe55992d22a6e19a7b360dda12c")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
