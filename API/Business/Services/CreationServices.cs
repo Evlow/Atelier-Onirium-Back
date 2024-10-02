53dd1442d634ed8c5f6a5b623568258d030daa79
Mathilde PEAUGER <mathilde.peauger@gmail.com>
2024-10-02 23:29:06 +0200

    update files

---
+++ API/Business/Services/CreationServices.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Business.DTO;
+using API.Data.RepositoryContract;
+using AutoMapper;
+
+namespace API.Business.Services
+{
+    public class CreationService
+    {
+        // Dépendances injectées pour accéder aux données de création et au mappage d'objets
+        private readonly ICreationRepository _creationRepository;
+        private readonly IMapper _mapper;
+
+        // Constructeur pour injecter les dépendances nécessaires
+        public CreationService(ICreationRepository creationRepository, IMapper mapper)
+        {
+            _creationRepository = creationRepository;
+            _mapper = mapper;
+        }
+
+        // Méthode asynchrone pour obtenir la liste des créations
+        // Utilise le repository pour récupérer les créations et les mapper en objets DTO
+        public async Task<List<CreationDTO>> GetCreationsAsync()
+        {
+            // Récupère les créations depuis la base de données de manière asynchrone
+            var creations = await _creationRepository.GetCreationsAsync().ConfigureAwait(false);
+
+            // Initialisation d'une liste de DTOs avec la même capacité que la liste des créations récupérées
+            List<CreationDTO> listCreationDTO = new List<CreationDTO>(creations.Count);
+
+            // Parcours des créations et mappage vers des objets CreationDTO
+            foreach (var creation in creations)
+            {
+                listCreationDTO.Add(_mapper.Map<CreationDTO>(creation));
+            }
+
+            // Retourne la liste des DTOs
+            return listCreationDTO;
+        }
+
+    }
+}
