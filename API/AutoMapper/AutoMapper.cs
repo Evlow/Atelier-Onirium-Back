df6ba57d2badbb3a7e86ce766556b83b05f122df
Mathilde PEAUGER <mathilde.peauger@gmail.com>
2024-10-02 23:20:31 +0200

    add AutoMapper

--- API/AutoMapper/AutoMapper.cs
+++ API/AutoMapper/AutoMapper.cs
@@ -2,11 +2,18 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using API.Business.DTO;
+using API.Entities;
+using AutoMapper;
 
 namespace API.AutoMapper
 {
-    public class AutoMapper
+    public class AutoMapper : Profile
     {
-        
+        public AutoMapper()
+        {
+            CreateMap<Creation, CreationDTO>().ReverseMap();
+
+        }
     }
 }
