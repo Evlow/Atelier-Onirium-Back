79862822983ad69fe3e52201716c0fe5c8ad2322
Mathilde PEAUGER <mathilde.peauger@gmail.com>
2024-10-09 19:55:28 +0200

    Add Basket Repo - Services - Controller

--- API/Data/RepositoryContract/IBasketRepository.cs
+++ API/Data/RepositoryContract/IBasketRepository.cs
@@ -8,6 +8,11 @@ namespace API.Data.RepositoryContract
 {
     public interface IBasketRepository
     {
-        Task<Basket> GetBasketAsync(string buyerId);
+       Task<Basket> GetBasketAsync(string buyerId);
+        Task<Basket> AddItemToBasketAsync(string buyerId, int productId, int quantity);
+        Task<bool> RemoveItemFromBasketAsync(string buyerId, int productId, int quantity);
+        Task<Basket> CreateBasketAsync(string buyerId);
+        Task<bool> DeleteBasketAsync(string buyerId);
+        Task<bool> SaveChangesAsync();
     }
 }
