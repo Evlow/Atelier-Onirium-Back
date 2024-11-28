4eba15fd501816866374d10e3d8602886649c024
Mathilde PEAUGER <mathilde.peauger@gmail.com>
2024-11-28 11:40:35 +0100

    Remove Id DTO

--- API/Business/DTO/CreationDTO.cs
+++ API/Business/DTO/CreationDTO.cs
@@ -9,6 +9,7 @@ namespace API.Business.DTO
     // Utilisé pour transférer les données entre les couches de l'application sans exposer les entités de la base de données directemen
     public class CreationDTO
     {
+    public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public string PictureUrl { get; set; }
