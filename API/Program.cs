925f8959665cd8cb6a14432a82e89eae48a4689c
Mathilde PEAUGER <mathilde.peauger@gmail.com>
2024-12-04 18:29:58 +0100

    Update program

--- API/Program.cs
+++ API/Program.cs
@@ -37,35 +37,64 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
             {
-                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                var jwtSecurityScheme = new OpenApiSecurityScheme
                 {
-                    Description = "Jwt auth header",
+
+                    BearerFormat = "JWT",
                     Name = "Authorization",
                     In = ParameterLocation.Header,
                     Type = SecuritySchemeType.ApiKey,
-                    Scheme = "Bearer"
-                });
-                c.AddSecurityRequirement(new OpenApiSecurityRequirement
-                {
+                    Description = "Put Bearer + token ",
+                    Reference = new OpenApiReference
+
+
                     {
-                        new OpenApiSecurityScheme
-                        {
-                            Reference = new OpenApiReference
-                            {
-                                Type = ReferenceType.SecurityScheme,
-                                Id = "Bearer"
-                            },
-                            Scheme = "oauth2",
-                            Name = "Bearer",
-                            In = ParameterLocation.Header
-                        },
-                        new List<string>()
+                        Id = JwtBearerDefaults.AuthenticationScheme,
+                        Type = ReferenceType.SecurityScheme,
+
+
                     }
-                });
+                };
+                c.AddSecurityDefinition(jwtSecurityScheme.Reference.Id, jwtSecurityScheme);
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+
+ {
+   { jwtSecurityScheme, Array.Empty<string>()
+ }}
+
+
+
+                );
+
+                //     c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                //     {
+                //         Description = "Jwt",
+                //         Name = "Authorization",
+                //         In = ParameterLocation.Header,
+                //         Type = SecuritySchemeType.ApiKey,
+                //         Scheme = "Bearer"
+                //     });
+                //     c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                //     {
+                //         {
+                //             new OpenApiSecurityScheme
+                //             {
+                //                 Reference = new OpenApiReference
+                //                 {
+                //                     Type = ReferenceType.SecurityScheme,
+                //                     Id = "Bearer"
+                //                 },
+                //                 Scheme = "oauth2",
+                //                 Name = "Bearer",
+                //                 In = ParameterLocation.Header
+                //             },
+                //             new List<string>()
+                //         }
+                //     });
             });
 
-builder.Services.AddIdentityCore<User>(opt=>
-{opt.User.RequireUniqueEmail = true;})
+builder.Services.AddIdentityCore<User>(opt =>
+{ opt.User.RequireUniqueEmail = true; })
 .AddRoles<IdentityRole>()
 .AddEntityFrameworkStores<AtelierOniriumContext>();
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
@@ -112,7 +141,7 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI(
-        c => {c.ConfigObject.AdditionalItems.Add("persistAuthorized", "true");}
+        c => { c.ConfigObject.AdditionalItems.Add("persistAuthorization", "true"); }
     );
 }
 
