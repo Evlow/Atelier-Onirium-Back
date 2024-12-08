28fbdd0631e1a28dfbadef89abc9d196c1e4bc9a
Mathilde PEAUGER <mathilde.peauger@gmail.com>
2024-12-08 02:16:15 +0100

    Update LoginDTO

--- API/Business/DTO/LoginDTO.cs
+++ API/Business/DTO/LoginDTO.cs
@@ -1,14 +1,18 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace API.Business.DTO
 {
     public class LoginDTO
-    {
-        public string UserName{get; set;} 
+    
+{
+    [Required(ErrorMessage = "Le nom d'utilisateur est requis.")]
+    public string UserName { get; set; }
 
-        public string Password {get; set;} 
-    }
+    [Required(ErrorMessage = "Le mot de passe est requis.")]
+    public string Password { get; set; }
+}
 }
